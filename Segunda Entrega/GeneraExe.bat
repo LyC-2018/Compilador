aef91ef1f71d21071ac458e14eb7e7ab938c36ee
leoavalos95 <leonelavalos.95@gmail.com>
2018-10-15 21:13:37 -0300

    Segunda Entrega

--- Segunda Entrega/GeneraExe.bat
+++ Segunda Entrega/GeneraExe.bat
@@ -4,13 +4,13 @@ c:\GnuWin32\bin\flex Lexico.l
 pause
 c:\GnuWin32\bin\bison -dyv Sintactico.y
 pause
-c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Primera.exe
+c:\MinGW\bin\gcc.exe lex.yy.c y.tab.c -o Segunda.exe
 pause
 pause
-Primera.exe Prueba.txt
+Segunda.exe Prueba.txt
 del lex.yy.c
 del y.tab.c
 del y.output
 del y.tab.h
-del Primera.exe
+del Segunda.exe
 pause
